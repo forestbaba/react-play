3c97938890b21f0a3488088daa392d93802d9e72
Faniran Olaitan <hawlahethan@gmail.com>
2020-07-30 17:05:15 +0100

    Head component bootstrapped

---
+++ src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.scss';
+import Logo from '../../assets/graphics/logo.jpg'
+const Header =(props)=>{
+
+    return(
+        <header>
+            <div className='wrap'>
+                <div className='logo'>
+                    <img src ={Logo} alt='logo'/>
+                </div>
+
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;
