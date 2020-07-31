63dbcb5eb721bbe9bea94ea3b87b2daee527dc07
Faniran Olaitan <hawlahethan@gmail.com>
2020-07-31 08:51:52 +0100

    redux implemented and ready for testing

---
+++ src/createStore.js
@@ -0,0 +1,8 @@
+import {createStore, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk';
+ import RootReducer from './reducers';
+
+
+ export const middleWares = [ReduxThunk];
+ export const createStoreWithMiddleWare = applyMiddleware(...middleWares)(createStore)
+ export const store = createStoreWithMiddleWare(RootReducer);
